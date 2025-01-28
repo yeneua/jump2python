432210bdbdf77bf9a018115c124abef422564bb8
yena <rladpsk1117@gmail.com>
2025-01-28 17:33:50 +0900

    1/28 05장 파이썬 날개 달기 - 모듈

---
+++ 05/mymod/mod2.py
@@ -0,0 +1,12 @@
+PI = 3.141592
+
+class Math:
+    def solv(self, r):
+        return PI * (r**2)
+    
+    def add(a,b):
+        return a+b
+
+def add(a,b):
+    result = a + b
+    return result
